f7dc46b3b72f897ce55a02ca3ae875264dba7d31
Tilak Basnet <tilak.basnet@outlook.com>
2024-03-27 20:55:33 +0100

    Has been added the Basic concept of the Rust programming

---
+++ basic/BasicConcepts.rs
@@ -0,0 +1,24 @@
+
+// This is a single-line comment
+/* 
+    This is a multi-line 
+    comment
+*/
+
+//***** variable *******//
+let x = 5 // immutable variable
+let mut y = 10 // mutable 
+let b: bool;
+
+// Primitive data types
+let a: i32 = 42; // 32-bit signed integer
+let b: f64 = 3.14; // 64-bit floating-point number
+let c: bool = true; // boolean
+let d: char = 'A'; // character
+let e: &str = "Hello, Rust!"; //String slice
+
+// Compound data typesa
+let tuple: (i32, f64, char ) = (42, 3.14, 'A'); // tuple
+let array: [i32; 5] = [1, 3, 4, 5]; // array
+
+
